9e5cbee2dc352a24b3083f965ac564f20cf1c9ba
Guilherme de Souza <54341560+HGuigit@users.noreply.github.com>
2022-03-08 21:41:07 -0300

    Added scrollbar logic and opacity

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -9,7 +9,29 @@ export class MainPageComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
+
+    // Estilização dinâmica da navbar com eventlistener
+    window.addEventListener('scroll',(event) => {
+    let elementClass = 'top-header'
+    let header = document.getElementsByClassName(elementClass).item(0) as HTMLElement
+    if(window.scrollY < 10){
+      header.style.backgroundColor = `rgba(0, 0, 0,${0})`
+    }
+    if(window.scrollY >= 10) {
+      header.style.backgroundColor = `rgba(0, 0, 0,${window.scrollY/250})`
+      if(window.scrollY > 250){
+        header.style.backgroundColor = `rgba(0, 0, 0,${1})`
+      }
+    }
+  });
+
   }
+
+  ngOnDestroy(): void{
+    window.removeEventListener('scroll' , () => null);
+  }
+
 
 }
