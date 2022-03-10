cd156cb8486e9c6eae47bded87b68e05c52110d7
Guilherme de Souza <54341560+HGuigit@users.noreply.github.com>
2022-03-09 23:17:20 -0300

    Added rocket scroll animation and footer

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { faFacebook, faGoogle, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-main-page',
@@ -7,15 +8,28 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  faFacebook = faFacebook;
+  faGoogle = faGoogle;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+
   constructor() { }
 
 
   ngOnInit(): void {
 
-    // Estilização dinâmica da navbar com eventlistener
+    //dynamic Styling of main page
+                                                                                                                                                         
+    let rocketMarginBottom : number = 0
+    let lastScrollTop : number = 0
+    let documentHeight : number = document.documentElement.scrollHeight
+  
     window.addEventListener('scroll',(event) => {
-    let elementClass = 'top-header'
+      
+      // Estilização dinâmica da navbar com eventlistener
+    let elementClass : string = 'top-header'
     let header = document.getElementsByClassName(elementClass).item(0) as HTMLElement
+
     if(window.scrollY < 10){
       header.style.backgroundColor = `rgba(0, 0, 0,${0})`
     }
@@ -25,11 +39,29 @@ export class MainPageComponent implements OnInit {
         header.style.backgroundColor = `rgba(0, 0, 0,${1})`
       }
     }
+    // Estilização de movimento do foguete
+    let rocketId : string = 'rocket-img'
+    let rocketImg = document.getElementById(rocketId) as HTMLElement
+    // Vai começar a mexer quando estiver no intervalo entre metade do primeiro retangulo e final dos segundo retangulo (primeira seção e no final da segunda seção)
+    if(window.scrollY > documentHeight/6  && window.scrollY < (documentHeight/3)*2 + documentHeight/6){ 
+       //detectando sentido do scroll
+       let st = document.documentElement.scrollTop
+
+       if (st > lastScrollTop){
+        rocketMarginBottom += 12
+        rocketImg.style.marginBottom = rocketMarginBottom + 'px' 
+      } else {
+        rocketMarginBottom -= 12
+        rocketImg.style.marginBottom = rocketMarginBottom + 'px' 
+      }
+      lastScrollTop = st;
+    }
+
   });
 
   }
 
-  ngOnDestroy(): void{
+  ngOnDestroy(){
     window.removeEventListener('scroll' , () => null);
   }
 
